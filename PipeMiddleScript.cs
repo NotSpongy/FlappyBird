8c37fd6fc2dc277c12101ff68b280606a1e5766a
Spongy <60718606+NotSpongy@users.noreply.github.com>
2025-05-15 17:32:31 -0700

    Update PipeMiddleScript.cs
    Removed two prompts

--- PipeMiddleScript.cs
+++ PipeMiddleScript.cs
@@ -4,13 +4,11 @@ public class PipeMiddleScript : MonoBehaviour
 {
     public LogicScript logic;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
